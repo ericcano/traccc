33acf65f95a925a04c63288a2ed5d4e7a242d548
Eric Cano <Eri.Cano@cern.ch>
2026-03-06 17:44:10 +0100

    Adds a single thread delegation class
    (intended for CUDA runtime single threaded access)

---
+++ device/cuda/include/traccc/cuda/utils/single_thread_delegation.hpp
@@ -0,0 +1,90 @@
+/** TRACCC library, part of the ACTS project (R&D line)
+ *
+ * (c) 2026 CERN for the benefit of the ACTS project
+ *
+ * Mozilla Public License Version 2.0
+ */
+
+#pragma once
+
+#include <atomic>
+#include <exception>
+#include <functional>
+#include <tbb/task_arena.h>
+#include <tbb/task_group.h>
+
+namespace traccc::cuda {
+  /// Base class for multiple implementations of a lamdba delegator.
+  ///
+  /// Main implementation takes a functional and exectues is in a single,
+  /// globally shared thread
+  class single_threaded_delegator {
+    public:
+    virtual void delegate(std::function<void()> func) = 0;
+  };
+
+
+  /// Derived class of single_threaded_delegator that does not propagate exceptions
+  /// back to the caller. The caller does not block, and the delegated task is executed asynchronously.
+  /// Exceptions handling is left to the TBB task scheduler, which will catch and log them (to be tested).
+  class single_threaded_delegator_fire_and_forget : public single_threaded_delegator {
+    public:
+    void delegate(std::function<void()> func) override {
+      m_arena.enqueue([this, func](){
+          m_group.run(func);
+      });
+    }
+
+    ~single_threaded_delegator_fire_and_forget() {
+      m_group.wait();
+    }
+
+    single_threaded_delegator_fire_and_forget() : m_arena(1) {}
+
+    private:
+    tbb::task_arena m_arena;
+    tbb::task_group m_group;
+  };
+
+  /// Synchronized variant of single_threaded_delegator that propagates
+  /// exceptions back to the caller.
+  ///
+  /// The caller blocks on a spinlock until the delegated task completes.
+  /// If the lambda threw, the exception is rethrown at the call site.
+  class single_threaded_delegator_sync {
+    public:
+    void delegate(std::function<void()> func) {
+      std::exception_ptr eptr = nullptr;
+      std::atomic<bool> done{false};
+
+      m_arena.enqueue([this, &func, &eptr, &done]() {
+        m_group.run([&func, &eptr, &done]() {
+          try {
+            func();
+          } catch (...) {
+            eptr = std::current_exception();
+          }
+          done.store(true, std::memory_order_release);
+        });
+      });
+
+      while (!done.load(std::memory_order_acquire)) {
+        // spinlock
+      }
+
+      if (eptr) {
+        std::rethrow_exception(eptr);
+      }
+    }
+
+    ~single_threaded_delegator_sync() {
+      m_group.wait();
+    }
+
+    single_threaded_delegator_sync() : m_arena(1) {}
+
+    private:
+    tbb::task_arena m_arena;
+    tbb::task_group m_group;
+  };
+}
